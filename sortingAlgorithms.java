d3ad06a370affdc18aa65265e69f5d8101725db3
daniele <danielefranzot23@gmail.com>
2022-12-18 13:33:34 +0100

    implementation of a simple sorting algorithm

--- sortingAlgorithms.java
+++ sortingAlgorithms.java
@@ -1,19 +1,19 @@
-public class sortingAlgorithms{
-	
-
-	public static void main(String[] args) {
-		int max = 20;
-		int min = 10;
-		int[] intArray = new int[(int)(Math.random() * (max - min + 1) + min)];
-
-		for (int i = 0; i < intArray.length ; i++) {
-			intArray[i] = (int)(Math.random() * (max + 1));
+public class SortingAlgorithms {
+	public static void simpleSorting(int[] arr) {
+		for (int i = 0; i < arr.length ; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[j] < arr[i]) {
+					swap(arr, i, j);
+				}
+			}
 		}
-
-		for (int i = 0; i < intArray.length ; i++) {
-			System.out.print(intArray[i]);
-		}
-		System.out.println();
-
 	}
+
+	public static int[] swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+		return arr;
+	}
+
 }
